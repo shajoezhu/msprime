f2d84e1adb808fab84d8486d2796a4918de40853
Joe Zhu <sha.joe.zhu@gmail.com>
2016-08-25 23:29:35 +0100

    getting the beta scaling factors

--- lib/hybridLambda_vs_msprime_beta.sh
+++ lib/hybridLambda_vs_msprime_beta.sh
@@ -25,7 +25,9 @@ program2="msprime"
 case=${program1}vs${program2}_beta
 
 ## Note that hybrid-Lambda scale branch length by 2Ne rather than 4Ne.
-hybrid-Lambda -spcu "(A:0,B:0);" -S 5 5 -num ${nRepeat} -o ${program1} -tmrca -bl -mu 0.1 -seg -sim_Si_num -mm 1.5
+#hybrid-Lambda -spcu "(A:0,B:0);" -S 5 5 -num ${nRepeat} -o ${program1} -tmrca -bl -mu 0.1 -seg -sim_Si_num -mm 1.5
+#hybrid-Lambda -spcu "(A:0,B:0);" -S 5 5 -num ${nRepeat} -o ${program1} -tmrca -bl -mu 0.006309427 -seg -sim_Si_num -mm 1.8
+hybrid-Lambda -spcu "(A:0,B:0);" -S 5 5 -num ${nRepeat} -o ${program1} -tmrca -bl -mu 3.981072 -seg -sim_Si_num -mm 1.1
 
 rearrange_hybridLambdaout $( grep "{ population = 0; time = 0.0; }" tmp.cfg | wc -l | cut -f1 -d' ')
 
